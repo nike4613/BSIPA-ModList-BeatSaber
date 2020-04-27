bcb5ff63a354ee0f6756cfe17a8c9925b125903b
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-04-26 21:13:37 -0500

    Implemented link modal

--- ModList/UI/ModInfoViewController.cs
+++ ModList/UI/ModInfoViewController.cs
@@ -1,11 +1,14 @@
 ﻿using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Parser;
 using BeatSaberMarkupLanguage.ViewControllers;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,6 +17,9 @@ namespace IPA.ModList.BeatSaber.UI
     [HotReload(PathMap = new[] { "C:\\", CompileConstants.SolutionDirectory })]
     internal class ModInfoViewController : BSMLAutomaticViewController
     {
+        [UIParams]
+        internal BSMLParserParams ParserParams;
+
         [UIValue("icon")]
         public Texture2D PluginIcon => plugin.Icon;
         [UIValue("name")]
@@ -61,12 +67,38 @@ namespace IPA.ModList.BeatSaber.UI
                 IconImage.texture = PluginIcon;
         }
 
+        [UIValue("link_url")]
+        public string CurrentLinkUrl { get; private set; } = "";
+
+        [UIValue("link_title")]
+        public string CurrentLinkTitle { get; private set; } = "";
+
+        [UIComponent("link-title-text")]
+        internal TextMeshProUGUI LinkTitleText;
+
         [UIAction("OnDescLinkPressed")]
         [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "BSML calls this")]
         private void OnDescriptionLinkPressed(string url, string title)
         {
             Logger.log.Debug($"Link to {url} ({title}) has been clicked");
 
+            CurrentLinkUrl = url;
+            CurrentLinkTitle = title ?? "";
+            NotifyPropertyChanged(nameof(CurrentLinkUrl));
+            NotifyPropertyChanged(nameof(CurrentLinkTitle));
+
+            LinkTitleText.enabled = !string.IsNullOrEmpty(CurrentLinkTitle);
+
+            ParserParams.EmitEvent("ShowLinkModal");
+        }
+
+        [UIAction("ConfirmOpenLink")]
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "BSML calls this")]
+        private void OpenLink()
+        {
+            Logger.log.Debug($"Link to {CurrentLinkUrl} ({CurrentLinkTitle}) has been confirmed");
+
+            Process.Start(CurrentLinkUrl);
         }
 
         protected override void DidActivate(bool firstActivation, ActivationType type)
